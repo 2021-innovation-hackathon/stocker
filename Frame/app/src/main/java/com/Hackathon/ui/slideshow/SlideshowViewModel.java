2cec3c3da3c3fd1456b76fa5566b90557013db45
HyunDel <pshyun1331@gmail.com>
2021-08-25 14:53:24 +0900

    어플리케이션 기본 틀

---
+++ Frame/app/src/main/java/com/Hackathon/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.Hackathon.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is investment fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
