16bc45b4b4c40dcc1ecc5a8c5f8e8be0799bce19
승철 차 <cscculture95@gmail.com>
2021-08-27 10:58:31 +0900

    마무리 작업

--- Frame/app/src/main/java/com/Hackathon/ui/home/Home_addMemo.java
+++ Frame/app/src/main/java/com/Hackathon/ui/home/Home_addMemo.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.Hackathon.DiaryDataAdapter;
 import com.Hackathon.DiaryItem;
 import com.Hackathon.DiaryItemDB;
 import com.Hackathon.R;
@@ -29,6 +31,8 @@ public class Home_addMemo extends AppCompatActivity {
     boolean flag;
 
     public List<DiaryItem> diaryItems = null;
+    DiaryDataAdapter diaryDataAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,20 +85,8 @@ public class Home_addMemo extends AppCompatActivity {
         t.start();
 
 
-        StringBuilder sb = new StringBuilder("");
-
-        if(diaryItems != null) {
-            for (DiaryItem item : diaryItems) {
-                if(item.date.equals(DateCode)) {
-                    sb.append("매수/매도? : " + item.isBuy + "\n" +
-                            "매수/매도? : " + item.categoryName + "\n" +
-                            "매수/매도? : " + item.price + "\n" +
-                            "매수/매도? : " + item.quantity + "\n");
-                }
-            }
-        }
-
-        comp.setText(sb.toString());
+        ListView listView = (ListView)findViewById(R.id.listView);
+        diaryDataAdapter = new DiaryDataAdapter(this, diaryItems);
     }
 
     public void backTohome(View view){
@@ -131,47 +123,9 @@ public class Home_addMemo extends AppCompatActivity {
         t.start();
         t.join();
 
-        StringBuilder sb = new StringBuilder("");
+        ListView listView = (ListView)findViewById(R.id.listView);
+        diaryDataAdapter = new DiaryDataAdapter(this, diaryItems);
 
-        if(diaryItems != null) {
-            for (DiaryItem item : diaryItems) {
-                System.out.println("null은 아닌듯");
-                System.out.println(item.date);
-                System.out.println(DateCode);
-                if(item.date.equals(DateCode)) {
-                    sb.append("매수/매도? : " + item.isBuy + "\n" +
-                            "매수/매도? : " + item.categoryName + "\n" +
-                            "매수/매도? : " + item.price + "\n" +
-                            "매수/매도? : " + item.quantity + "\n");
-                }
-            }
-        } else {
-            System.out.println("왜 null인데");
-        }
-
-        comp.setText(sb.toString());
+        listView.setAdapter(diaryDataAdapter);
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_add_memo, menu);
-//
-//       if(getSupportActionBar() != null){
-//           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//           getSupportActionBar().setHomeAsUpIndicator(R.drawable.left_arrow);
-//       }
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch(item.getItemId()){
-//            case R.id.add_memo:
-//                Toast.makeText(this,"addMemo executed.",Toast.LENGTH_LONG).show();
-//                break;
-//            case R.id.home:
-//                finish();
-//                break;
-//        }
-//        return true;
-//    }
 }
